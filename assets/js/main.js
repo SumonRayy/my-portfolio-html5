181b578cebaa5bd665ba8b992a1203139903de9f
SumonRayy <sumonrayy@gmail.com>
2020-11-28 14:12:41 +0530

    First Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,31 @@
+// jtjxhrgillpdbxdb
+
+document.querySelector(".contact-form").addEventListener("submit", submitForm);
+
+
+function submitForm(e) {
+	console.log(e);
+
+	let name = document.querySelector(".c-name").value;
+	let email = document.querySelector(".c-email").value;
+	let message = document.querySelector(".c-message").value;
+
+	sendEmail(name, email, message);
+
+}
+
+function sendEmail(name, email, message) {
+	Email.send({
+		Host: "smtp.gmail.com",
+		Username: "sumonrayy@gmail.com",
+		Password: "jtjxhrgillpdbxdb",
+		To: "sumonrayy@gmail.com",
+		From: "sumonrayy@gmail.com",
+		Subject: `Website Message from ${name}`,
+		Body: `Name: ${name} <br />Email: ${email} <br />Message: ${message}`
+	}).then((message) => alert("Message has been sent!"))
+}
+
 /*
 	Massively by HTML5 UP
 	html5up.net | @ajlkn
